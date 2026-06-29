979de18145b9ee4058900bdc9f2cd5a715e929d5
Anuraj singh <ajsinghindolia@gmail.com>
2026-06-29 23:45:59 +0530

    Time: 1 ms (100%), Space: 44 MB (72.73%) - LeetHub

---
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String findLatestTime(String s) {
+
+        char[] time=s.toCharArray();
+
+        if(time[0]=='?'){
+            if(time[1]=='?' || time[1]<='1'){
+                time[0]='1';
+            }else{
+                time[0]='0';
+            }
+        }
+
+        if(time[1]=='?'){
+            if(time[0]=='0'){
+               time[1]='9';
+            }else{
+
+                time[1]='1';
+            }
+        }
+
+        if(time[3]=='?'){
+            time[3]='5';
+        }
+
+        if(time[4]=='?'){
+         time[4]='9';
+        }
+
+        return new String(time);
+        
+    }
+}
