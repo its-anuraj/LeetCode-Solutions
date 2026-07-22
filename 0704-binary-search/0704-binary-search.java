7bb5ee10225f02e4f9ad1fa8f6106a8ea139f531
Anuraj singh <ajsinghindolia@gmail.com>
2026-07-23 01:34:14 +0530

    Time: 0 ms (100%), Space: 48.3 MB (69.36%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,26 +3,16 @@ class Solution {
         int n = nums.length;
         int first = 0;
         int last = n - 1;
-        boolean isAsc = nums[first] < nums[last];
 
         while (first <= last) {
             int mid = first + (last - first) / 2;
-
-            if (target == nums[mid])
+            if (target > nums[mid]) {
+                first = mid + 1;
+            } else if (target < nums[mid]) {
+                last = mid - 1;
+            } else {
                 return mid;
 
-            if (isAsc) {
-                if (target > nums[mid]) {
-                    first = mid + 1;
-                } else {
-                    last = mid - 1;
-                }
-            } else {
-                if (target < nums[mid]) {
-                    first = mid + 1;
-                } else {
-                    last = mid - 1;
-                }
             }
 
         }
